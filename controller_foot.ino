39f44509ff7f1e5c0fdb486b9dfded1930207b1b
Ejdzi2 <81420520+Ejdzi2@users.noreply.github.com>
2021-12-15 13:25:27 +0100

    Update controller_foot.ino

--- controller_foot.ino
+++ controller_foot.ino
@@ -1,19 +1,17 @@
 #include <MIDI_Controller.h>
 
 /*
-This is an example of the "Digital" class of the MIDI_controller library.
-Connect a push buttons to digital pin 2. Connect the other pin of the button to the ground, 
-a pull-up resistor is not necessary, because the internal one will be used. 
-This button will play MIDI note C4 when pressed.
-Map it in your DAW or DJ software.
+This is practical using of the "Digital" class of the MIDI_controller library.
+Conected push buttons to digital pins  and the ground of arduino pro micro. 
 
-Written by tttapa, 08/09/2017
-https://github.com/tttapa/MIDI_controller
+This buttons will play MIDI notes when pressed.
+
+Written originally by tttapa, 08/09/2017 modified by Ejdzi2
 */
 
 #include <MIDI_Controller.h> // Include the library
 
-uint8_t channel = 5;
+uint8_t channel = 5; //initial midi channel
 const uint8_t velocity = 0b1111111; // Maximum velocity (0b1111111 = 0x7F = 127)
 const uint8_t c = 24;
 const uint8_t cis = 25;
@@ -29,7 +27,8 @@ const uint8_t ais = 34;
 const uint8_t h = 35; 
 const uint8_t c1 = 36;    // Note number 48 is defined as  C2 in the MIDI specification
 
-// Create a new instance of the class 'Digital', called 'button', on pin 2, that sends MIDI messages with note 'C4' (60) on channel 1, with velocity 127
+// Create a new instance of the class 'Digital', called 'button', on pins 3-10 & 16-19, that sends MIDI messages with notes on channel (initialy 5), with velocity 127
+// and on pins 2,21 that sends notes off on channel (initialy 5) - velocity 0
 
  
 Digital buttons[] = {
@@ -46,20 +45,17 @@ Digital buttons[] = {
   {15, ais, channel, velocity},
   {18, h, channel, velocity},
   {19, c1, channel, velocity},
-  {21, 127, channel, 0},  //notes off 
-  {2, 127, channel, 0},   //notes off
+  {21, 127, channel, 0},  //all notes off and program change button (defined in Digital.cpp) - works when pushed simultanously with notes buttons
+  {2, 127, channel, 0},   //notes off and channel change button (defined in Digital.cpp) - works when pushed simultanously with notes buttons
 };
 
 //DigitalCC off(2, MIDI_CC::All_Notes_Off, channel); // pin 2,  mozna ustawic jako all notes off - wyłączony
 
-// A bank that shifts each button one octave (i.e. twelve semitones)
+// A bank that shifts each button one octave (12 semitones)
 Bank octave(12); 
-
-// A bank selector with a momentary push button on pin 0 and an LED on pin 11-nie swieci, 1-swieci
+// A bank selector with a momentary push button on pin 0 and an LED on pin 11-no light (original), 1-light (up transposed)
 BankSelector bs(octave, {0}, {11, 1, 1, 1, 1});
- 
-
-void setup() {  // Add the note buttons to the bank, changing the address (i.e. note number) - zmiana oktaw
+ void setup() {  // Add the note buttons to the bank, changing the address (note number) - zmiana oktaw
   octave.add(buttons, Bank::CHANGE_ADDRESS);
   }
 
